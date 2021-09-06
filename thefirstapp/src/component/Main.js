81295e3017247cf8bcb0c22eda9662b37ee1c8b7
DuhaAbuGhazleh <duhamahmoud0@gmail.com>
2021-09-06 10:31:48 +0300

    edit

--- thefirstapp/src/component/Main.js
+++ thefirstapp/src/component/Main.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import UserData from './UserData'
+import HornedBeast1 from './image/HornedBeast1.jpg'
+import HornedBeast2 from './image/HornedBeast2.jpg'
+
  class Main extends Component {
     render() {
-        let HornedBeast=[{title:"Rhino Family",discription:"Mother (or father) rhino with two babies"},
-        {title:"Unicorn Head",discription:"Someone wearing a creepy unicorn head mask"}]
+        let HornedBeast=[{title:"Rhino Family",image:HornedBeast1,discription:"Mother (or father) rhino with two babies"},
+        {title:"Unicorn Head",image:HornedBeast2,discription:"Someone wearing a creepy unicorn head mask"}]
         return (
             <>
             
             {
                HornedBeast.map(element=>{
-                    return <UserData title={element.title}  discription={element.discription}/>
+                    return <UserData title={element.title}  discription={element.discription} imagesrc={element.image}/>
                 })
             }
             
--- thefirstapp/src/component/UserData.js
+++ thefirstapp/src/component/UserData.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react'
     render() {
         return (
             <div>
-               <h2>title: {this.props.title}</h2>
+               <h2>title : {this.props.title}</h2>
+               <img src={this.props.imagesrc} alt={this.props.title} title='horned image'/>
                 <p>discription:{this.props.discription} </p> 
             </div>
         )
