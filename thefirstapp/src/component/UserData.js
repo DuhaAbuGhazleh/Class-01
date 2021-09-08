ee2c42c422b2508034cbb0e54d1429f43b06df2b
DuhaAbuGhazleh <duhamahmoud0@gmail.com>
2021-09-08 10:23:15 +0300

    edit

--- thefirstapp/src/component/UserData.js
+++ thefirstapp/src/component/UserData.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react'
  class UserData extends Component {
-     constructor(props){
+    constructor(props){
         super(props);
         this.state={
-            
-             stateVote:this.props.UserDataVote,
+          like:0,  
          }
      }
      riseVote=()=>{
         this.setState({
-          UserDataVote:this.state.stateVote+1
+          like:this.state.like+1
             
 
         })
      }
+
     render() {
         return (
             <div>
                <h2>title : {this.props.title}</h2>
                <img onClick={this.riseVote} src={this.props.image_url} alt={this.props.title} title='horned image'/>
              
-               {/* <UserData childVote={this.state.stateVote}/> */}
+               <h2>vote: {this.state.like}</h2> 
                 <p>description:{this.props.description} </p> 
                 <p>keyword:{this.props.keyword} </p> 
                 <p> horns:{this.props.horns} </p> 
-               <p>vote: {this.state.stateVote}</p> 
+             
 
             </div>
         )
